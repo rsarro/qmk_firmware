81e4d4251b39547e46be75d078fd5dc75d950c08
Rafael Sarro <rafael.sarro@gmail.com>
2017-08-31 22:40:58 +0100

    Adjust build.sh after repository reorganization

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker run -e keymap=rsf_osx -e keyboard=ergodox --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware
-docker run -e keymap=default_osx -e keyboard=ergodox --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware
+docker run -e keymap=rsf_osx -e keyboard=ergodox_ez --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware
+docker run -e keymap=default_osx -e keyboard=ergodox_ez --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware
