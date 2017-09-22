b3baa821e59c55daafd2fe421acdbf90f58e88dd
Rafael Sarro <rafael.sarro@gmail.com>
2017-09-22 15:25:05 +0100

    Add build for submodules

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+docker run  --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware make git-submodule
 docker run -e keymap=rsf_osx -e keyboard=ergodox_ez --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware
 docker run -e keymap=default_osx -e keyboard=ergodox_ez --rm -v /Users/rafael/hobbies/qmk_firmware:/qmk:rw edasque/qmk_firmware
